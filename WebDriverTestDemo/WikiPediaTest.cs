70c427501217f3e2ad074cbea71c06b8df9c1bb3
trankov <trankov@BSF-NB-023>
2022-05-18 14:00:05 +0300

    Add project files.

---
+++ WebDriverTestDemo/WikiPediaTest.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+driver.Url = "https://wikipedia.org";
+
+var searchField = driver.FindElement(By.Id("searchInput"));
+searchField.Click();
+searchField.SendKeys("QA");
+searchField.SendKeys(Keys.Enter);
+//driver.Quit();
